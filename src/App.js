383dffb21e59b953ee46bfcb5690c07266827972
DavidHughesJr <hughes014@yahoo.com>
2022-08-11 17:22:43 -0500

    net test

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         <Route path="/popular" element={<PopularPage setSearch={setSearch}/>} />
         <Route path="/:category/:id" element={<DetailsPage saves={saves} setSaves={setSaves} setSearch={setSearch} />} />
         <Route path="/person/:id" element={<PersonPage setSearch={setSearch} />} />
-        <Route path="/streaming/:navItems/:navItems" element={<StreamPage setSearch={setSearch} />} />
+        <Route path="/streaming/:navItems/:navId" element={<StreamPage setSearch={setSearch} />} />
         <Route path="/genre/:genreName/:genreId" element={<GenresPage setSearch={setSearch} />} />
         <Route path="/showall" element={<ShowAllPage setSearch={setSearch} />} />
         <Route path="/trending" element={<TrendingPage setSearch={setSearch} />} />
